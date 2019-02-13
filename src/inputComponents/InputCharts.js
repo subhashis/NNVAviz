4b709ce0845344d41b3187c65662b812c7ab6fbb
Harvey Li <aide0740@gmail.com>
2019-02-13 11:53:31 -0500

    button functions and s/l file

--- src/inputComponents/InputCharts.js
+++ src/inputComponents/InputCharts.js
@@ -3,14 +3,24 @@ import Parameter from './Parameter';
 import Preview from './Preview';
 
 export default class InputCharts extends Component {
+    constructor(props){
+        super(props);
+        this.state={};
+    }
+    handleTableClick(data){
+        console.log(data);
+        this.setState({previewData:data});
+    }
     render(){
         return (
             <div id="inputs" >
                 <Parameter 
                     run = {this.props.request}
+                    previewData = {this.props.previewData}
+                    tableClick = {(d)=>{this.handleTableClick(d)}}
                 />
                 <Preview 
-                    previewData = {this.props.previewData}
+                    previewData = {this.state.previewData?this.state.previewData:this.props.previewData}
                     radius={150} 
                     size={400} 
                     valueLen={400}
