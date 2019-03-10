e8c297f3ac8dd87139d323b2cfe17edb88dfef3a
Harvey Li <aide0740@gmail.com>
2019-03-09 21:32:58 -0500

    stage

--- src/MatrixView.js
+++ src/MatrixView.js
@@ -197,19 +197,19 @@ class MatrixView extends Component {
             }
         }
 
-        function kernelDensityEstimator(kernel, X) {
-            return function(V) {
-                return X.map(function(x) {
-                    return [x, d3.mean(V, function(v) { return kernel(x - v); })];
-                });
-            };
-          }
+        // function kernelDensityEstimator(kernel, X) {
+        //     return function(V) {
+        //         return X.map(function(x) {
+        //             return [x, d3.mean(V, function(v) { return kernel(x - v); })];
+        //         });
+        //     };
+        //   }
           
-        function kernelEpanechnikov(k) {
-            return function(v) {
-                return Math.abs(v /= k) <= 1 ? 0.75 * (1 - v * v) / k : 0;
-            };
-        }
+        // function kernelEpanechnikov(k) {
+        //     return function(v) {
+        //         return Math.abs(v /= k) <= 1 ? 0.75 * (1 - v * v) / k : 0;
+        //     };
+        // }
 
     }
     render(){
